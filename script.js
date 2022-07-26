c2e45f39429e6f1545fbf1fe390737fd4dbc2849
Anna <momannuta@gmail.com>
2022-07-26 13:41:52 +0300

    new

---
+++ script.js
@@ -0,0 +1,29 @@
+let arr = [];
+function clear(){
+    arr = [];    
+    document.querySelector(".sortArr").innerHTML= "";
+    document.querySelector('.sum').innerHTML ="";
+}
+function sumInput() {    
+    document.querySelector(".sortArr").innerHTML = arr.sort((a, b) => a-b);
+    let filteredArr = arr.filter(Boolean);
+    function count(filteredArr) {
+    let sum = 0;
+    filteredArr.forEach(function(item){
+        sum +=item
+    })
+    return sum;
+    }
+    count(filteredArr);
+    document.querySelector('.sum').innerHTML = `Сумма: ${count(filteredArr)}`;    
+}
+    
+
+function saveValue() {  
+    let input = document.querySelector('input').value;
+    if (input === "" || input === null || !isFinite(input)){    
+    sumInput();
+    }    
+    arr.push(+input);    
+}
+sumInput();
